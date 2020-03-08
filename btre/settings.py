1c2b99e629b4ad8337f560247a13faf11bdb46e0
Andrey Egorov <andreyegorov1985@gmail.com>
2020-03-08 23:38:34 +0300

    changed database to postgresql

--- btre/settings.py
+++ btre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from btre.secrets import SECRET_KEY
+from btre.secrets import SECRET_KEY, POSTGRES_USER, POSTGRES_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': 'localhost'
     }
 }
 
